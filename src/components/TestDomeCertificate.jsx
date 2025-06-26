cfa40970e2842c7a258880a7cfda0b9c8595860d
aniketjawarkar <aniketnot2026@gmail.com>
2025-06-26 22:28:29 +0530

    basic Css , Pdf, Loader, ScrollEffect , Particle Effects

---
+++ src/components/TestDomeCertificate.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import AOS from "aos";
+
+const TestDomeCertificate = (props) => {
+  useEffect(() => {
+    const stylesheet =
+      "https://www.testdome.com/content/source/stylesheets/embed.css";
+    const link = document.createElement("link");
+    link.href = stylesheet;
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.media = "screen,print";
+    document.getElementsByTagName("head")[0].appendChild(link);
+    AOS.init();
+  }, []);
+
+  return (
+    <a
+      href={props.url}
+      target="_blank"
+      className="testdome-certificate-stamp gold"
+      data-aos="fade-up"
+      rel="noreferrer"
+    >
+      <span className="testdome-certificate-name">Aniket Jawarkar</span>
+      <span className="testdome-certificate-test-name">{props.skill}</span>
+      <span className="testdome-certificate-card-logo">
+        TestDome
+        <br />
+        Certificate
+      </span>
+    </a>
+  );
+};
+
+export default TestDomeCertificate;
